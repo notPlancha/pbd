197ca78c0b9792621dab237a5a06760a76598faa
notPlancha <andre.plancha@hotmail.com>
2023-04-05 00:55:52 +0100

    pipeline started

--- utils/imports.py
+++ utils/imports.py
@@ -2,6 +2,7 @@ import os
 os.environ["PYARROW_IGNORE_TIMEZONE"] = "1"
 import pyspark
 from pyspark.sql.functions import *
+from pyspark.sql import Window
 import pyspark.pandas as ps
 from IPython import display
 import numpy as np
