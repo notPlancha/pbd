0ace7a346c4decc924c759eba9039af082259290
notPlancha <andre.plancha@hotmail.com>
2023-04-07 03:51:14 +0100

    modelos feitos

--- utils/start_spark.py
+++ utils/start_spark.py
@@ -5,7 +5,9 @@ sparkBuilder = SparkSession.builder \
     .appName('pbd') \
     .config("spark.sql.repl.eagerEval.enabled", True) \
     .config("spark.sql.repl.eagerEval.truncate", 50) \
-    .config("spark.sql.repl.eagerEval.maxNumRows", 50)
+    .config("spark.sql.repl.eagerEval.maxNumRows", 50) \
+    .config("spark.driver.memory","4g") \
+    .config("spark.executer.memory","4g")
 spark: SparkSession = sparkBuilder.getOrCreate()
 
 def reload(spark):
