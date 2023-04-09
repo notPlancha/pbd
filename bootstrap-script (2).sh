ee886bad79eba03163c1d9666e0599d08e028f55
notPlancha <andre.plancha@hotmail.com>
2023-04-09 22:39:26 +0100

    1.0

---
+++ bootstrap-script (2).sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo python3 -m pip install -U pip
+sudo python3 -m pip install -U setuptools
+sudo python3 -m pip install -U pyspark
+sudo python3 -m pip install -U seaborn
+sudo python3 -m pip install -U matplotlib
+sudo python3 -m pip install -U numpy
+sudo python3 -m pip install -U setuptools
+sudo python3 -m pip install -U dataclassy
+
+
+
